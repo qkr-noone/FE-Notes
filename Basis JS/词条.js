920bfb103924317f7182c99c425d1d61b9574bb0
qkr-noone <1666272287@qq.com>
2020-04-10 00:55:09 +0800

    code 原型链 和 深拷贝

--- Basis JS/词条.js
+++ Basis JS/词条.js
@@ -1,10 +1,41 @@
 `
 活动对象（AO）
-变量对象（VO） 
+变量对象（VO）
 执行上下文(execution context) https://juejin.im/post/58ec3cc944d90400576a2cdc
 函数生命周期 https://pic4.zhimg.com/v2-88a513ba2ce27b5b41ec6e188d07b30f_r.jpg
 `
 
 // 闭包 https://juejin.im/post/5b081f8d6fb9a07a9b3664b6  https://juejin.im/post/5b1fa77451882513ea5cc2ca
 // 作用域 作用域链 https://juejin.im/post/5afb0ae56fb9a07aa2138425
+
 // 原型 原型链
+`
+1.原型对象和构造函数有何关系？
+在JavaScript中，每当定义一个函数数据类型(普通函数、类)时候，都会天生自带一个prototype属性，这个属性指向函数的原型对象。
+当函数经过new调用时，这个函数就成为了构造函数，返回一个全新的实例对象，这个实例对象有一个__proto__属性，指向构造函数的原型对象。
+https://user-gold-cdn.xitu.io/2019/10/20/16de955a81892535?imageslim
+
+2.能不能描述一下原型链？
+JavaScript对象通过__proto__ 指向父类对象，直到指向Object对象为止，这样就形成了一个原型指向的链条, 即原型链。
+https://user-gold-cdn.xitu.io/2019/10/20/16de955ca89f6091?imageView2/0/w/1280/h/960/format/webp/ignore-error/1
+
+
+对象的 hasOwnProperty() 来检查对象自身中是否含有该属性
+使用 in 检查对象中是否含有某个属性时，如果对象中没有但是原型链中有，也会返回 true
+`
+
+// https://juejin.im/post/58f94c9bb123db411953691b#heading-2
+`简单回顾下构造函数,原型和实例的关系:
+
+每个构造函数(constructor)都有一个原型对象(prototype),原型对象都包含一个指向构造函数的指针,而实例(instance)都包含一个指向原型对象的内部指针.
+`
+
+`
+确定原型和实例的关系
+怎么去判断原型和实例的这种继承关系呢? 方法一般有两种.
+第一种是使用 instanceof 操作符, 只要用这个操作符来测试实例(instance)与原型链中出现过的构造函数,结果就会返回true.
+第二种是使用 isPrototypeOf() 方法, 同样只要是原型链中出现过的原型,isPrototypeOf() 方法就会返回true.
+console.log(Object.prototype.isPrototypeOf(instance))
+`
+// 拓展
+// 继承 -> Class -> new 
--- Classic JS/深浅拷贝.js
+++ Classic JS/深浅拷贝.js
@@ -11,13 +11,14 @@ Array.prototype.slice()
 */
 
 // for in 的浅拷贝实现方式
-function shallowCopy(src) {
-  let d = {}
-  for (const prop in src) {
-    if (src.hasOwnProperty(prop)) {
-      d[prop] = src[prop]
+function shallowCopy(target) {
+  let cloneTarget = {}
+  for (const prop in target) {
+    if (target.hasOwnProperty(prop)) {
+      cloneTarget[prop] = target[prop]
     }
   }
+  return cloneTarget
 }
 
 let ate = [1, 2, { user: 'kobe', max: 10000 }]
@@ -32,4 +33,126 @@ console.log(ate, ate3)
 
 // 深拷贝的实现方式
 JSON.parse(JSON.stringify())
-  /* 如何写出一个惊艳面试官的深拷贝? https://juejin.im/post/5d6aa4f96fb9a06b112ad5b1  https://juejin.im/post/5bc1ae9be51d450e8b140b0c */
+  /* 如何写出一个惊艳面试官的深拷贝? 
+  https://juejin.im/post/5d6aa4f96fb9a06b112ad5b1  优秀
+  https://juejin.im/post/5bc1ae9be51d450e8b140b0c
+  */
+
+// 基础版本 for in  + 递归
+`
+如果是原始类型，无需继续拷贝，直接返回
+如果是引用类型，创建一个新的对象，遍历需要克隆的对象，将需要克隆对象的属性执行深拷贝后依次添加到新对象上
+`
+function deepBaseCopy(target) {
+  if (typeof target === 'object') {
+    let cloneTarget = {}
+    for (const key in target) {
+      cloneTarget[key] = deepBaseCopy(target[key])
+    }
+    return cloneTarget
+  } else {
+    return target
+  }
+}
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: 'ConardLi',
+  field4: {
+    child: 'child',
+    child2: {
+      child2: 'child2'
+    }
+  }
+};
+let n = deepBaseCopy(target)
+n.field4.child2.child2 = 800
+console.log(target, n)
+`缺陷 未考虑数组`
+
+function deepObjArrCopy(target) {
+  if (typeof target === 'object') {
+    let clone = Array.isArray(target) ? [] : {}
+    for (const key in target) {
+      clone[key] = deepObjArrCopy(target[key])
+    }
+    return clone
+  } else {
+    return target
+  }
+}
+const target2 = {
+  field1: 1,
+  field2: undefined,
+  field3: 'ConardLi',
+  field4: {
+    child: 'child',
+    child2: {
+      child2: [1, 2]
+    }
+  }
+};
+let n2 = deepObjArrCopy(target2)
+n2.field4.child2.child2[0] = 800
+console.log(target2, n2)
+
+`const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8]
+};
+target.target = target
+deepObjArrCopy(target)
+`
+`递归进入死循环导致栈内存溢出`
+
+`原因就是上面的对象存在循环引用的情况，即对象的属性间接或直接的引用了自身的情况`
+`解决循环引用问题，我们可以额外开辟一个存储空间，来存储当前对象和拷贝对象的对应关系，当需要拷贝当前对象时，先去存储空间中找，有没有拷贝过这个对象，如果有的话直接返回，如果没有的话继续拷贝，这样就巧妙化解的循环引用的问题。
+这个存储空间，需要可以存储key-value形式的数据，且key可以是一个引用类型，我们可以选择Map这种数据结构：
+
+检查 map 中有无克隆过的对象
+有 - 直接返回
+没有 - 将当前对象作为key，克隆对象作为value进行存储
+继续克隆
+`
+function deepMapCopy(target, map = new Map()) {
+  if (typeof target === 'object') {
+    let clone = Array.isArray(target) ? [] : {}
+    if (map.get(target)) {
+      return map.get(target)
+    }
+    map.set(target, clone)
+    for (const key in target) {
+      clone[key] = deepMapCopy(target[key], map)
+    }
+    return clone
+  } else {
+    return target
+  }
+}
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: {
+    child: 'child'
+  },
+  field4: [2, 4, 8]
+};
+target.target = target
+console.log(deepMapCopy(target))
+// 执行没有报错，且target属性，变为了一个Circular类型，即循环应用的意思
+
+`WeakMap 代替 Map`
+`WeakMap 对象是一组键/值对的集合，其中的键是弱引用的。其键必须是对象，而值可以是任意的`
+`什么是弱引用呢？
+  在计算机程序设计中，弱引用与强引用相对，是指不能确保其引用的对象不会被垃圾回收器回收的引用。
+  一个对象若只被弱引用所引用，则被认为是不可访问（或弱可访问）的，并因此可能在任何时刻被回收。
+`
+function deepWeakMapCopy(target, map = new WeakMap()) {} // 其他都一样
+// 以上仅仅是一版合格的 深拷贝
+
+`性能优化`  // 待续。。。
+`for in 在遍历时效率是非常低的 三种循环for、while、for in的执行效率 while的效率是最好`
