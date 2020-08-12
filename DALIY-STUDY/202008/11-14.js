904d47b651b79c7e9b047292d48c780c1d55b98b
qkr-noone <1666272287@qq.com>
2020-08-12 22:58:38 +0800

    实现 getOwnPropertyDescriptors __proto__

--- DALIY-STUDY/202008/11-14.js
+++ DALIY-STUDY/202008/11-14.js
@@ -65,3 +65,66 @@ let combine = function (n, k) {
   return ret
 }
 console.log(combine(4, 2))
+
+// 实现 __proto__  阮一峰 ES6标准入门 __proto__ 属性 P166
+Object.defineProperty(Object, '__proto__', {
+  get () {
+    let _thisObj = Object(this)
+    return Object.getPrototypeOf(_thisObj)
+  },
+  set (proto) {
+    if (this === undefined || this === null) {
+      throw new TypeError()
+    }
+    if (!isObject(this)) {
+      return undefined
+    }
+    if (!isObject(proto)) {
+      return undefined
+    }
+    let status = Reflect.setPrototypeOf(this, proto)
+    if (!status) {
+      throw new TypeError()
+    }
+  }
+})
+function isObject (value) {
+  return Object(value) === value
+}
+Object.__proto__ = { 1: 1 }
+console.log(Object.getPrototypeOf(Object)) //  { '1': 1 }
+
+// console.log(Object.getPrototypeOf({__proto__: null})) //  { null }
+
+// 1. 扩展运算符的解构赋值，不能复制继承自原型对象的属性
+// 2. 单纯的解构赋值，可以读取对象继承的属性
+const o = Object.create({x: 1, y: 2})
+o.z = 3
+console.log(Object.getPrototypeOf(o)) // { x: 1, y: 2 }
+
+let { x, ...newObj } = o
+let { y, z } = newObj
+console.log(x, y, z) // 1 undefined 3
+console.log(newObj) // { z: 3 }
+/* 
+代码中，变量x是单纯的解构赋值，所以可以读取对象o继承的属性；
+变量y和z是扩展运算符的解构赋值，只能读取对象o自身的属性，
+所以变量z可以赋值成功，变量y取不到值
+*/
+
+/* 
+书中的错误
+ES6 规定，变量声明语句之中，如果使用解构赋值，扩展运算符后面必须是一个变量名，
+而不能是一个解构赋值表达式，所以上面代码引入了中间变量newObj，如果写成下面这样会报错。
+*/
+// let { x, ...{ y, z } } = o;
+// SyntaxError: ... must be followed by an identifier in declaration contexts
+
+// 实现 getOwnPropertyDescriptors(obj)
+function getOwnPropertyDescriptors(obj) {
+  const result = {}
+  for (const key of Reflect.ownKeys(obj)) {
+    obj[key] = Object.getOwnPropertyDescriptor(obj, key)
+  }
+  return result
+}
