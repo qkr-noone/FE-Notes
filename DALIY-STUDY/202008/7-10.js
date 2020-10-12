725da27e0d4c7a4e678ba0bbca5f29e0d735b0a3
qkr-noone <1666272287@qq.com>
2020-10-12 17:29:42 +0800

    recode 10.12

--- DALIY-STUDY/202008/7-10.js
+++ DALIY-STUDY/202008/7-10.js
@@ -75,10 +75,14 @@ const insertSort = arr => {
 
 console.log(insertSort([2, 77, 0, 99, 12, 1]))
 
-// 快速排序
+// 快速排序 基本思想是：通过一趟排序将要排序的数据分割成独立的两部分，
+// 其中一部分的所有数据都比另外一部分的所有数据都要小，
+// 然后再按此方法对这两部分数据分别进行快速排序，
+// 整个排序过程可以递归进行，以此达到整个数据变成有序序列。
 function quickSort(arr) {
   const list = arr.slice(); //保证函数为纯函数
   if (list.length <= 1) return list
+  // 在原有的数组中把它移除
   const point = list.splice(0, 1)[0] // 选第一个作为基数
   const left = []
   const right = []
